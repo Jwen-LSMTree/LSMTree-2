4cdfae03ab5c433b723c430e0d332dc4d5a561f3
euna <christinejung10@daum.net>
2023-08-22 21:36:36 +0900

    fix: Util - compact

--- src/Util.cpp
+++ src/Util.cpp
@@ -1,27 +1,26 @@
 #include "Util.h"
+
 #include <algorithm>
 
-std::vector<Entry> Util::compact(const std::vector<std::vector<Entry>> &inputs) {
+using namespace std;
+
+bool compareKey(Entry &e1, Entry &e2);
+
+vector<Entry> Util::compact(const vector<vector<Entry>> &inputs) {
     size_t n = inputs.size();
-    std::vector<Entry> ret;
-    std::vector<size_t> poses(n);
-    size_t nonEmpty = 0;
-    for (size_t i = 0; i < n; ++i)
-        if (!inputs[i].empty())
-            ++nonEmpty;
-    while (nonEmpty) {
-        uint64_t key = UINT64_MAX;
-        for (size_t i = 0; i < n; ++i)
-            if (poses[i] < inputs[i].size())
-                key = std::min(key, inputs[i][poses[i]].key);
-        std::string value;
-        for (size_t i = 0; i < n; ++i)
-            if (poses[i] < inputs[i].size() && inputs[i][poses[i]].key == key) {
-                value = inputs[i][poses[i]++].value;
-                if (poses[i] == inputs[i].size())
-                    --nonEmpty;
-            }
-        ret.emplace_back(key, value);
+    vector<Entry> ret;
+    vector<size_t> poses(n);
+
+    vector<Entry> entries;
+    for (const vector<Entry> &input: inputs) {
+        for (const Entry &entry: input) {
+            entries.push_back(entry);
+        }
     }
-    return ret;
+    sort(entries.begin(), entries.end(), compareKey);
+    return entries;
+}
+
+bool compareKey(Entry &e1, Entry &e2) {
+    return e1.key < e2.key;
 }
