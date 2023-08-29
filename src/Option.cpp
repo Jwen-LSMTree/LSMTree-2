2974a6bac1e25d17dbe92621629ebe2c0e0dc692
euna <christinejung10@daum.net>
2023-08-29 22:38:37 +0900

    fix: 'Option::' 누락 수정

--- src/Option.cpp
+++ src/Option.cpp
@@ -4,21 +4,21 @@ const bool Option::COMPACTION = true;
 
 const uint64_t Option::SST_SPACE = (uint64_t) 2 * 1024 * 1024;
 
-const uint64_t Option::BLOCK_SPACE = (uint64_t)4 * 1024;
+const uint64_t Option::BLOCK_SPACE = (uint64_t) 4 * 1024;
 
 const uint64_t Option::NZ_NUM = 6;
 
-const uint64_t Option::Z_SPACE = (uint64_t)9 * 1024 * 1024;
+const uint64_t Option::Z_SPACE = (uint64_t) 9 * 1024 * 1024;
 
 const uint64_t Option::NZ_SPACES[] = {
-    (uint64_t)64 * 1024 * 1024,
-    (uint64_t)512 * 1024 * 1024,
-    (uint64_t)4096 * 1024 * 1024,
-    (uint64_t)32768 * 1024 * 1024,
-    (uint64_t)262144 * 1024 * 1024,
-    UINT64_MAX};
+        (uint64_t) 64 * 1024 * 1024,
+        (uint64_t) 512 * 1024 * 1024,
+        (uint64_t) 4096 * 1024 * 1024,
+        (uint64_t) 32768 * 1024 * 1024,
+        (uint64_t) 262144 * 1024 * 1024,
+        UINT64_MAX};
 
-const uint64_t BLOOM_FILTER_SIZE = 1024;
+const uint64_t Option::BLOOM_FILTER_SIZE = 1024;
 
 const char *const Option::Z_NAME = "/L0";
 
