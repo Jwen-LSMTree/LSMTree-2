dd7ef85e16374351054a3d32506d91fa4209eecd
JungWoo <rlawjddn103@naver.com>
2023-09-15 00:32:54 +0900

    temp

--- test/latency/snapshot.cpp
+++ test/latency/snapshot.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-const int TEST_COUNT = 3;
-const int ENTRY_COUNT = 100000;
-const int DUPLICATE_COUNT = 3;
+const int TEST_COUNT = 1;
+const int ENTRY_COUNT = 10000;
+const int DUPLICATE_COUNT = 30;
 const string alphabets[5] = {"a", "b", "c", "d", "e"};
 
 KVStore *store;
